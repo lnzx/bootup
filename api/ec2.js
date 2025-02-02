1034a4660080dc0cf1122952b6c893226dce5a57
lnzx <lnx@tutamail.com>
2025-02-02 21:26:15 +0800

    fix ec2

--- api/ec2.js
+++ api/ec2.js
@@ -17,25 +17,16 @@ export default async function handler(req, res) {
 
   const rsp = await client.send(command)
 
-  if (rsp.Reservations.length > 0) {
-    // 提取第一个Reservation中的第一个实例
-    const reservation = rsp.Reservations[0]
-    const instance = reservation[0].Instances[0]
-    console.log(instance.InstanceId) // 输出: i-0abcd1234efgh5678
-    console.log(instance.InstanceType) // 输出: t2.micro
-    console.log(instance.State.Name) // 输出: running
-    console.log(instance.PublicIpAddress) // 输出: 123.45.67.89
-    console.log(instance.LaunchTime.toISOString()) // 输出: running
-    const instanceData = {
-      InstanceId: instance.InstanceId,
-      InstanceType: instance.InstanceType,
-      State: instance.State.Name,
-      PublicIpAddress: instance.PublicIpAddress,
-      LaunchTime: instance.LaunchTime ? instance.LaunchTime.toISOString() : '', // 处理 LaunchTime 可能为空的情况
-    }
-
-    res.status(200).json(instanceData)
+  if (rsp.Reservations) {
+    const instance = rsp.Reservations[0].Instances[0] // 提取第一个Reservation中的第一个实例
+    res.status(200).json({
+      id: instance.InstanceId,
+      type: instance.InstanceType,
+      state: instance.State.Name,
+      ip: instance.PublicIpAddress,
+      time: instance.LaunchTime ? instance.LaunchTime.toISOString() : '', // 处理 LaunchTime 可能为空的情况
+    })
+  } else {
+    res.status(404).json({ message: 'No instances found.' })
   }
-
-  res.status(404).json({ message: 'No instances found.' })
 }
