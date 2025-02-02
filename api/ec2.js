df98b1696d0920a0cec7180bab7eb16ec4d512c5
lnzx <lnx@tutamail.com>
2025-02-02 21:40:02 +0800

    fix

--- api/ec2.js
+++ api/ec2.js
@@ -3,9 +3,8 @@ import { EC2Client, DescribeInstancesCommand } from '@aws-sdk/client-ec2'
 const command = new DescribeInstancesCommand({})
 
 export default async function handler(req, res) {
-  const region = req.query.region
-  const key = req.query.key
-  const secret = req.query.secret
+  // 从查询参数中获取 region、key 和 secret
+  const { region, key, secret } = req.query
 
   const client = new EC2Client({
     region: region,
@@ -17,15 +16,22 @@ export default async function handler(req, res) {
 
   const rsp = await client.send(command)
 
-  if (rsp.Reservations) {
-    const instance = rsp.Reservations[0].Instances[0] // 提取第一个Reservation中的第一个实例
-    res.status(200).json({
-      id: instance.InstanceId,
-      type: instance.InstanceType,
-      state: instance.State.Name,
-      ip: instance.PublicIpAddress,
-      time: instance.LaunchTime ? instance.LaunchTime.toISOString() : '', // 处理 LaunchTime 可能为空的情况
-    })
+  // 检查 Reservations 和 Instances 是否存在
+  if (rsp.Reservations && rsp.Reservations.length > 0) {
+    const instances = rsp.Reservations.flatMap((reservation) => reservation.Instances) // 将所有实例扁平化
+    if (instances.length > 0) {
+      const instance = instances[0] // 取第一个实例
+
+      const data = {
+        id: instance.InstanceId,
+        type: instance.InstanceType,
+        state: instance.State.Name,
+        ip: instance.PublicIpAddress,
+        time: instance.LaunchTime ? instance.LaunchTime.toISOString() : '', // 处理 LaunchTime 可能为空的情况
+      }
+
+      res.status(200).json(data)
+    }
   } else {
     res.status(404).json({ message: 'No instances found.' })
   }
